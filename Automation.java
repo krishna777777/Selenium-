106dbd2b7f502f7379d63acab17aea80d75e073f
krishna777777 <77561971+krishna777777@users.noreply.github.com>
2023-08-30 13:23:33 +0530

    Update and rename  to Automation.java

---
+++ Automation.java
@@ -0,0 +1,19 @@
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
+
+
+
+	}
+
+}
