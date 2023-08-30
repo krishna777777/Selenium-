ff33f1f50cb52d3cb5fcba809fcbec13c4b46e77
krishna777777 <77561971+krishna777777@users.noreply.github.com>
2023-08-30 13:19:36 +0530

    Update and rename  to mydemodeummy.java

---
+++ mydemodeummy.java
@@ -0,0 +1,16 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class qualysdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         WebDriver driver = new ChromeDriver();
+         
+		
+	
+
+}
